86a6a1190601cfbd1d69208a0311ac2d4f0ab5fd
ashishmandal542-byte <ashishmandal542@gmail.com>
2026-04-20 05:06:25 +0530

    Add cocktail fetching and display functionality
    Fetch cocktail data from JSON and display in the DOM.

--- Js App.js
+++ Js App.js
@@ -1,1 +1,17 @@
+fetch('cocktails.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("cocktails");
 
+    data.forEach(cocktail => {
+      const card = document.createElement("div");
+
+      card.innerHTML = `
+        <img src="${cocktail.image}" width="200"/>
+        <h3>${cocktail.name}</h3>
+        <p>${cocktail.description}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  });
